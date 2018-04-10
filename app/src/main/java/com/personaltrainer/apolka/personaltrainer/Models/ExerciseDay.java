13e5053f51d1c7b58469534dcecdb6f3b0d8f8ca
apolkaszasz <31844307+apolkaszasz@users.noreply.github.com>
2018-04-10 17:25:40 +0300

    program item

--- app/src/main/java/com/personaltrainer/apolka/personaltrainer/Models/ExerciseDay.java
+++ app/src/main/java/com/personaltrainer/apolka/personaltrainer/Models/ExerciseDay.java
@@ -1,21 +1,22 @@
 package com.personaltrainer.apolka.personaltrainer.Models;
 
+import java.io.Serializable;
 import java.util.List;
 
-public class ExerciseDay {
+public class ExerciseDay implements Serializable {
 
-    private String dayId;
+    private int dayId;
     private List<String> exercises;
 
     ExerciseDay(){}
-    ExerciseDay(String dayId, List<String> exerciseList){
+    ExerciseDay(int dayId, List<String> exerciseList){
         this.dayId = dayId;
         this.exercises = exerciseList;
     }
 
-    public String getDayId(){return this.dayId;}
+    public int getDayId(){return this.dayId;}
     public List<String> getExercises(){return  this.exercises;}
 
-    public void setDayId(String dayId){this.dayId = dayId;}
+    public void setDayId(int dayId){this.dayId = dayId;}
     public void setExercises(List<String> exercises){this.exercises = exercises;}
 }
--- app/src/main/java/com/personaltrainer/apolka/personaltrainer/ProgramItem.java
+++ app/src/main/java/com/personaltrainer/apolka/personaltrainer/ProgramItem.java
@@ -2,19 +2,32 @@ package com.personaltrainer.apolka.personaltrainer;
 
 import android.content.Context;
 import android.content.Intent;
+import android.media.audiofx.AudioEffect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.personaltrainer.apolka.personaltrainer.Models.Exercise;
 import com.personaltrainer.apolka.personaltrainer.Models.ExerciseDay;
 import com.personaltrainer.apolka.personaltrainer.Models.Program;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class ProgramItem extends AppCompatActivity {
 
     private static final String TAG = "ProgramItem";
+
+
+
+    private TextView programName;
+    private TextView programDescription;
+    private TextView programDurationInWeeks;
+    private TextView programWorkoutdays;
+
 
     private static final String PROGRAM_OBJ_KEY = "Program_Obj_Key";
     public static Intent getStartIntent(Context context, Program program) {
@@ -28,6 +41,36 @@ public class ProgramItem extends AppCompatActivity {
 
         Intent i = getIntent();
         Program program = (Program) i.getSerializableExtra(PROGRAM_OBJ_KEY);
+
+        List<String> dayList =  new ArrayList<String>();
+        List<String> workoutDayList =  new ArrayList<String>();
+
+        dayList.add("Monday");
+        dayList.add("Tuesday");
+        dayList.add("Wednesday");
+        dayList.add("Thursday");
+        dayList.add("Friday");
+        dayList.add("Saturday");
+        dayList.add("Sunday");
+
+        for (int j =0;j<7;j++){
+            if (program.getDaysListPerWeek().get(j)==1){
+                workoutDayList.add(dayList.get(j));
+            }
+        }
+
+        programName = (TextView)findViewById(R.id.ProgramNameTextView);
+        programName.setText(program.getName());
+
+        programDescription = (TextView)findViewById(R.id.ProgramDescriptionValue);
+        programDescription.setText(program.getDescription());
+
+        programDurationInWeeks = (TextView)findViewById(R.id.ProgramDurationInWeeksTextViewValue);
+        programDurationInWeeks.setText(program.getDurationInWeeks()+"");
+
+        programWorkoutdays = (TextView)findViewById(R.id.ProgramWorkoutDaysTextViewValue);
+        programWorkoutdays.setText(TextUtils.join(", ",workoutDayList));
+
         Log.d(TAG, "...........Name is: "+program.getName());
         Log.d(TAG,"Description is: "+program.getDescription());
         Log.d(TAG, "Duration in weeks"+program.getDurationInWeeks());
@@ -36,6 +79,6 @@ public class ProgramItem extends AppCompatActivity {
         Log.d(TAG, "Days: "+daysListPerWeek.get(0)+" "+daysListPerWeek.get(1)+" "+daysListPerWeek.get(2)+" "+daysListPerWeek.get(3)+" "
         +daysListPerWeek.get(4)+" "+daysListPerWeek.get(5)+" "+daysListPerWeek.get(6)+" ");
 
-        //Log.d(TAG,"Day "+exerciseList.get(0).getDayId()+"ex 1 "+exerciseList.get(0).getExercises().get(0));
+        Log.d(TAG,"Day "+exerciseList.get(0).getDayId()+"ex 1 "+exerciseList.get(0).getExercises().get(0));
     }
 }
